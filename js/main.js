5ba9be3d0c7593098670d24f3003ad4e6d47233b
zhimmat <zeyad.himmat@live.com>
2016-03-18 17:15:37 -0400

    added new border-radius

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $html.on('keydown', function () {
   var $newBall = $('<div>');
 
   $newBall.addClass('ball');
-   $body.append($newBall);
-   $newBall.css('left', Math.random() * 1000);
-   $newBall.css('top', Math.random() * 1000);
- });
+  $body.append($newBall);
+  $newBall.css('left', Math.random() * 1000);
+  $newBall.css('top', Math.random() * 1000);
+});
