b840a887fec371dee5c40302a5f8b054e7dbdf5a
zhimmat <zeyad.himmat@live.com>
2016-03-18 17:13:29 -0400

    added more code in js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ var $html = $('html');
 
 $html.on('keydown', function () {
   var $newBall = $('<div>');
+
+  $newBall.addClass('ball');
+   $body.append($newBall);
+   $newBall.css('left', Math.random() * 1000);
+   $newBall.css('top', Math.random() * 1000);
+ });
